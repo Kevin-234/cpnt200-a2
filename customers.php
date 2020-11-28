8f28db9b5f0451606f767d826c828b72b6822ec4
Akari Taguchi <kevintaguchi0@gmail.com>
2020-11-27 18:06:53 -0700

    added comment

--- customers.php
+++ customers.php
@@ -1,7 +1,7 @@
 <?php
   
   include('includes/config.php');
-  
+
   // get id
   $id = $_GET['id'];
 
@@ -21,7 +21,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Customer Personal Information </title>
+    <title>Customer Personal Information</title>
     <!-- css -->
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/reset.css">
